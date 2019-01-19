7996098e5279fa1839477f86f65ad7bad562b632
Tomas <tcarneado@gmail.com>
2019-01-19 17:55:12 +0100

    service name typo

--- deploy/teamcity_update_service_test.sh
+++ deploy/teamcity_update_service_test.sh
@@ -6,4 +6,4 @@
 #./teamcity_update_service_test.sh
 #*****************************************
 
-docker service update --force --image 192.168.99.100:55000/ftspa/ui:ci-${UI_BUILD_NUMBER-1} ftspa
+docker service update --force --image 192.168.99.100:55000/ftspa/ui:ci-${UI_BUILD_NUMBER-1} fintech_test_ftspa
